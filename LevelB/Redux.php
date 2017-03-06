27378cc7167481d3c12274e84bf7afba4e366a66
robinduval <leyoda@gmail.com>
2017-03-06 10:37:26 +0100

    Update Redux.php

--- LevelB/Redux.php
+++ LevelB/Redux.php
@@ -16,10 +16,42 @@ class Redux
      *
      * @return string
      */
-    public function getReductedNumber()
+  public function getReductedNumber()
+    {
+        $result = $number;
+        $step = 0;
+        while (9 < $result) {
+            $numb = strval($result);
+            $len = strlen($numb);
+            $result = 0;
+            for ($i = 0;$i < $len;++$i) {
+                $result += $numb[$i];
+            }
+        }
+
+        return $result;
+    }
+
+    public function getReductedNumber2()
     {
         $result = $this->number;
-        // @TODO
+        $step = 0;
+
+        while (9 < $result) {
+            $result = $this->reduct(strval($result));
+        }
+
+        return $result;
+    }
+
+    private function reduct($number)
+    {
+        $len = strlen($number);
+        $result = 0;
+        for ($i = 0; $i < $len; ++$i) {
+            $result += $number[$i];
+        }
+
         return $result;
     }
 };
